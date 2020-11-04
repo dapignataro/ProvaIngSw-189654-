128d4275fbc2eb9e4ceae800a27312d56b9789d0
daniele <danielepignataro98@gmail.com>
2020-11-04 11:05:59 +0100

    prova

---
+++ ProvaIngsw/src/main/java/it/unical/ingsw2020/ProvaIngsw/App.java
@@ -0,0 +1,13 @@
+package it.unical.ingsw2020.ProvaIngsw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ProvaIngsw/src/test/java/it/unical/ingsw2020/ProvaIngsw/AppTest.java
@@ -0,0 +1,20 @@
+package it.unical.ingsw2020.ProvaIngsw;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
